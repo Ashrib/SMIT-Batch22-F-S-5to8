088b1e31321566ceccf67ccfb82edfe70a5b895c
Arham <arhamali63@gmail.com>
2026-06-28 20:12:27 +0500

    js variables

--- JS/Basics/app.js
+++ JS/Basics/app.js
@@ -1,5 +1,50 @@
-alert("hello world!"); 
-alert("welcome!");
+// alert("hello world!"); 
+// alert("welcome!");
+// window.alert("hello");
+
+/// variables for string
+// data-type
+
+
+/// create variable -> declare
+var username = "user12345";  // initial value / initialization
+// alert(username);
+
+
+var user_email;// declaration only
+
+user_email = "abc@gmail.com";  ////  assign a value to a variable
+
+console.log(user_email);
+user_email = "no value";
+
+console.log(user_email) /// re-assign / override
+
+
+var greet = "welcome to this site!";
+console.log(greet);
+
+
+/// variables for number
+var num1 = 10;   /// data-type ---> number 
+console.log(num1);
+
+var num2 = 20, num3, num4 = 0;
+num3 = 30;
+
+var a ;
+
+console.log("hello");
+
+/// complier vs interpreter
+
+//// high lang convert to low lang 
+
+
+
+// VS code --> code editor?
+/// VS ---> IDE
+
 
 
 
