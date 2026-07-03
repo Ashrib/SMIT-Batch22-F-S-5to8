3e4dfc04298fa2b65695e26c91bc7f789d4893c4
Ashrib <asharib920@gmail.com>
2026-07-03 19:54:54 +0500

    number variables, concatenations, math expressions

--- JS/Basics/app.js
+++ JS/Basics/app.js
@@ -26,19 +26,94 @@ console.log(greet);
 
 
 /// variables for number
-var num1 = 10;   /// data-type ---> number 
+var num1 = 10.40;   /// data-type ---> number 
 console.log(num1);
 
 var num2 = 20, num3, num4 = 0;
 num3 = 30;
 
-var a ;
+var newNum = num2 + 100; // 120
+console.log(newNum);
 
-console.log("hello");
+newNum = newNum + num2 + num3 + 10;
+console.log(newNum);
+
+// alert(200);  /// number convert into string
+
+var total = 15 - 5 * 10 / 5; //// dmas rule
+console.log(total); 
+
+var modulusResult = 50 % 100;
+console.log(modulusResult)
+
+
+var obtMarks = 450;
+var totalMarks = 500;
+
+var percentage = (obtMarks / totalMarks) * 100;
+
+
+var exp = (30 - 60) / ((70 *(4-3)) + 55);
+
+
+var firstName = 'ali ';
+var lastName = 'khan';
+
+console.log("welcome " + firstName + lastName);
+
+// console.log("20" + 20 + 10);
+
+// console.log(20 + '60' + 10 + 80 + '50' );
+
+console.log("30" * "10" + '5' + '-5');  /// +
+
+
+// user interaction -- ?
+// alert
+
+// ask user for the input
+
+var user_name = prompt("enter you name");
+var user_email = prompt("enter you email");
+console.log(user_name);
+
+/// js --> html
+
+
+
+
+
+
+// %  modulus
+//// data types
+// "abc" ---> string
+// 30 ---> number
+// true/false ---> boolean
+// undefined
+// null
+
+
+
+// var x = null;
+
+
+
+
+
+
+
+
+
+
 
 /// complier vs interpreter
 
 //// high lang convert to low lang 
+
+
+
+
+
 
 
 
