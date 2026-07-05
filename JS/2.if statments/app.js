3719484a8545a9de4bcff4c30ed15eb866addf0e
Ashrib <asharib920@gmail.com>
2026-07-05 19:51:14 +0500

    if statments

---
+++ JS/2.if statments/app.js
@@ -0,0 +1,82 @@
+// var userInput = window.prompt("enter your name" , "xyz");
+// console.log(userInput);
+
+
+/// prompt input is always string
+// var userAge = prompt("enter your age.");
+// console.log(userAge);
+
+var age = 15;
+
+if(age == 20) { /// block
+    console.log("only when the condition matches");
+}
+
+var city = 'karachi';
+
+// if(city == 'Karachi') console.log("yes.");
+
+var feePaid = true;
+
+if(feePaid == true){
+    console.log('you are allowed.')
+    
+}
+
+
+// if(30 === '30'){
+//     console.log(true);
+// }
+
+
+// if(feePaid !== true ){
+//     console.log("you are not allowed!");
+// }
+
+// var userCity = prompt("enter your city");
+// /// karachi 
+
+// if(userCity !== 'karachi'){
+//     console.log("not avaiable");
+// }
+
+
+
+
+
+if(age > 17){
+    console.log('okay.')
+}
+else{
+    console.log('not okay.')
+}
+
+
+
+
+// var percentage = 70;
+
+// if(percentage >= 80){
+//     console.log("grade A");
+// }
+// else if(percentage >= 70){
+//     console.log("grade B");
+// }
+// else if(percentage >= 60){
+//     console.log("grade C");
+// }
+// else if(percentage >= 50){
+//     console.log("grade D");
+// }
+// else{
+//     console.log("grade F");
+// }
+
+
+
+/// 5 subjects --> 100
+// total 500
+
+
+var numInput = +prompt('enter maths marks');
+console.log(numInput)
